4a370056d60f58b6281739ed3e6067bc454fba10
Randy <rsm5139@rsc64dot1x-42.ems.psu.edu>
2017-02-17 12:35:48 -0500

    Finished some work on cdo_mis_timestamp. Not done yet

--- cdo_mis_timestamp.sh
+++ cdo_mis_timestamp.sh
@@ -6,7 +6,10 @@
 #        Randy Miller
 #        rsm5139@psu.edu
 
-# last modified on 2017-02-14
+# last modified on 2017-02-16
+
+# This checks for missing timestamps in a NetCDF file. The cdo library is required to use this. Leaps days are ignored by defualt due to some model output skipping leap days. If timestamps are irregular, some other validation method is recomended.
+# Note: due to differences in the way Macs use the 'date' command, this will give errors on a Mac. 
 
 source uniqList.sh
 
@@ -14,11 +17,15 @@ source uniqList.sh
 function cdo_mis_timestamp()
 {
   # Initialize variables
-  local ts=""
-  local new=0
-  local old=0
-  local time_step=0
-  local flex_u=0 && flex_l=0
+  local                ts="" # Array for 'cdo showtimestamp' output
+  local       new_str_date=0 # Datestring
+  local       old_str_date=0 # Datestring
+  local                new=0 # Date in seconds
+  local                old=0 # Date in seconds
+  local          time_step=0 # Seconds between timestamps
+  local             ld=false # Leap day flag
+  local               tmp="" # For temp values
+  local    predicted_date="" # The predicted value for the next timestamp
   local h_mess="$(cat <<-EOF
 cdo_mis_timestamp <NetCDF file>
 
@@ -26,6 +33,8 @@ This functions checks for missing timestamps in a NetCDF file
 
 Positional Arguments:
 $1  name of a NetCDF file
+Flags:
+[ -l ] Don't ignore leap days
 Return Codes:
 0   normal return: no errors
 1   insufficient arguments
@@ -43,38 +52,51 @@ EOF
     return 1
   }
   
+  # Check for flag
+  [ "$1" == "-l" ] && {
+    ld=true
+    shift
+  }
+  
   # Check for CDO command
   cdo --version > /dev/null 2>&1 || {
     echo "Missing CDO library" >&2
     return 127
   }
   
+  # Get the timestamp list using 'cdo showtimestamp'
   ts=$(cdo showtimestamp "$1" 2>/dev/null) || {
     echo "Problem using 'cdo timestamp' on $1. Check that file exists and is a NetCDF file." >&2
     return 3
   }
+  
+  # uniqList is a function that returns a list of only uniques values, and will return 2 if there were duplicates
   ts=$(uniqList "$ts" " ")
   [ $? -eq 2 ] && { echo "Warning: duplicates found"; }
+  
+  # Start loop through the timestamps, checking that each new step is the expected value. Some checks are in place in case the value is not the expected value, but should otherwise be correct. (Example, monthly datasets aren't usually equally spaced due to their being varying numbers of days in a month.)
   for i in ${ts[@]}; do
-    new=$(date -d"$i" '+%s')
-    [ $old -eq 0 ] && { old=$new; continue; }
+    new_str_date=$(date -d"$i" --rfc-3339=seconds)
+    
+    # This only triggers on the first loop
+    [ $old_str_date -eq 0 ] && { old_str_date="$new_str_date"; continue; }
+    
+    # Get the seconds version of the dates
+    old=$(date -d"$old_str_date" '+%s')
+    new=$(date -d"$new_str_date" '+%s')
+    
+    # This only triggers on the second loop
     [ $time_step -eq 0 ] && { 
       time_step=$(( new - old ))
-      flex_u=$(( time_step + time_step / 5 ))
-      flex_l=$(( time_step - time_step / 5 ))
-      old=$new
+      [ $time_step -le 0 ] && return 2
       continue
     }
-    diff=$(( new - old ))
-    old=$new
-    [ $diff -lt $flex_u ] && [ $diff -gt $flex_l ] && continue
-    # This may just be a missing leap day. Check and pass it true.
-    [[ "$i" =~ [0-9]{4}-03-01.* ]] && {
-      diff=$(( diff - 86400 ))
-      [ $diff -lt $flex_u ] && [ $diff -gt $flex_l ] && continue
-    }
-    echo "Error at $i"
-    return 2
+    
+    # Use date string manipulation to predict the next timestamp
+    tmp=$(( old + time_step ))
+    predicted_date=$(date -d @$tmp --rfc-3339=seconds)
+    echo "predicted: $predicted_date"
+    echo "actual:    $new_str_date"
   done
   
   return 0
