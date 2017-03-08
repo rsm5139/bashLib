b960b52abb3f3504e4539cfa9edd4962f04f706c
Randy <rsm5139@rsc64dot1x-42.ems.psu.edu>
2017-03-08 09:52:06 -0500

    Edits

--- cdo_dup_timestamp.sh
+++ cdo_dup_timestamp.sh
@@ -6,7 +6,10 @@
 #        Randy Miller
 #        rsm5139@psu.edu
 
-# last modified on 2017-02-14
+# last modified on 2017-02-20
+
+# This checks for duplicate timestamps in a NetCDF file. The cdo library is required to use this. 
+# The process is to print out all of the timestamps, then test the length of the list before and after duplicates are removed. Another function called 'uniqList' is required.
 
 source uniqList.sh
 
@@ -15,7 +18,7 @@ function cdo_dup_timestamp()
 {
   # Initialize variables
   local ts=""
-  local h_mess="$(cat <<-EOF
+  local h_mess="
 cdo_dup_timestamp <NetCDF file>
 
 This functions checks for duplicate timestamps in a NetCDF file
@@ -28,8 +31,7 @@ Return Codes:
 2   duplicate timestamps were found
 3   file error
 127 missing cdo library
-EOF
-  )"
+"
 
   # Check for 1 argument
   [ "$#" -lt 1 ] && {
@@ -45,11 +47,13 @@ EOF
     return 127
   }
   
+  # Get the timestamp list using 'cdo showtimestamp'
   ts=$(cdo showtimestamp "$1" 2>/dev/null) || {
     echo "Problem using 'cdo timestamp' on $1. Check that file exists and is a NetCDF file." >&2
     return 3
   }
   
+  # Use uniqList command. The commard returns 2 if there are duplicates. The return code is then passed of as the retrun code of this function.
   uniqList "$ts" " " > /dev/null
   return $?
 }
--- cdo_mis_timestamp.sh
+++ cdo_mis_timestamp.sh
@@ -6,7 +6,7 @@
 #        Randy Miller
 #        rsm5139@psu.edu
 
-# last modified on 2017-02-16
+# last modified on 2017-02-20
 
 # This checks for missing timestamps in a NetCDF file. The cdo library is required to use this. Leaps days are ignored by defualt due to some model output skipping leap days. If timestamps are irregular, some other validation method is recomended.
 # Note: due to differences in the way Macs use the 'date' command, this will give errors on a Mac. 
@@ -26,7 +26,7 @@ function cdo_mis_timestamp()
   local             ld=false # Leap day flag
   local               tmp="" # For temp values
   local    predicted_date="" # The predicted value for the next timestamp
-  local h_mess="$(cat <<-EOF
+  local h_mess="
 cdo_mis_timestamp <NetCDF file>
 
 This functions checks for missing timestamps in a NetCDF file
@@ -41,8 +41,7 @@ Return Codes:
 2   missing timestamps were found
 3   file error
 127 missing cdo library
-EOF
-  )"
+"
 
   # Check for 1 argument
   [ "$#" -lt 1 ] && {
@@ -79,7 +78,7 @@ EOF
     new_str_date=$(date -d"$i" --rfc-3339=seconds)
     
     # This only triggers on the first loop
-    [ $old_str_date -eq 0 ] && { old_str_date="$new_str_date"; continue; }
+    [ "$old_str_date" == "0" ] && { old_str_date="$new_str_date"; continue; }
     
     # Get the seconds version of the dates
     old=$(date -d"$old_str_date" '+%s')
@@ -89,14 +88,43 @@ EOF
     [ $time_step -eq 0 ] && { 
       time_step=$(( new - old ))
       [ $time_step -le 0 ] && return 2
+      old_str_date="$new_str_date"
       continue
     }
     
     # Use date string manipulation to predict the next timestamp
     tmp=$(( old + time_step ))
     predicted_date=$(date -d @$tmp --rfc-3339=seconds)
-    echo "predicted: $predicted_date"
-    echo "actual:    $new_str_date"
+    
+    # Is the new timestamp within a 11% error margin? Then pass it
+    # Note: 11% is the worst-case scenario for how much regularly-spaced data can be off while technically being accurate. It's the error margin between a 28 day month and a 31 day month
+    [ $(( new + time_step / 9 )) -gt $tmp ] && [ $(( new - time_step / 9 )) -lt $tmp ] && {
+      old_str_date="${new_str_date}"
+      continue
+    }
+    
+    # Is this a missing leap day? We can pass or fail the check depending on user flag '-l'
+    $ld || {
+      [[ "${predicted_date:0:10}" =~ [0-9]{4}-02-29 ]] && [[ "${new_str_date:0:10}" =~ [0-9]{4}-03-01 ]] && {
+      old_str_date="${new_str_date}"
+      continue
+      }
+    }
+    
+    # Leap days could still affect annual data or data with longer sampling frequencies. A date trick could be used here to increment by year
+    # Note: 31536000 is the number of seconds in a year. 
+    [ 31536000 -le $time_step ] && {
+      tmp=$(( time_step / 31536000 ))
+      predicted_date=$(date -d "$old_str_date+$tmp years" --rfc-3339=seconds)
+      [ "${predicted_date:0:4}" == "${new_str_date:0:4}" ] && {
+        old_str_date="${new_str_date}"
+        continue
+      }
+    }
+    
+    # If we are here, it looks like it's a missing timestamp
+    echo "Error before $new_str_date" >&2
+    return 2
   done
   
   return 0
